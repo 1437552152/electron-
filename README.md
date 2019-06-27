2d2149f70059b82eb609a7a68f8218cea7f7c3e8
yfye100 <111111>
2019-06-27 10:32:09 +0800

    提交

---
+++ README.md
@@ -0,0 +1,6 @@
+# electronDemo
+electron 入门demo
+
+运行：
+E:\NodeStu\demo>npm install
+E:\NodeStu\demo>npm start
---
+++ js/click.js
@@ -0,0 +1,14 @@
+﻿const { ipcRenderer, remote, shell } = window.require('electron');
+let minobj=document.getElementById('min');
+let maxobj=document.getElementById('max');
+let closeobj=document.getElementById('close')
+minobj.onclick=function(){
+   remote.getCurrentWindow().show();
+    remote.getCurrentWindow().minimize(); 
+}
+maxobj.onclick=function(){
+    remote.getCurrentWindow().maximize(); 
+}
+closeobj.onclick=function(){
+    remote.getCurrentWindow().hide();
+}
